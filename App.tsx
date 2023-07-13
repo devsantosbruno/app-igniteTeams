9191a98b9539bf3e150029a4827415ac2f9c26b0
bruno santos <devbrunosantos@gmail.com>
2023-07-13 20:46:40 -0300

    feat: using loading component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
+
+import { Loading } from "./src/components/Loading";
 import { Groups } from "./src/screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
