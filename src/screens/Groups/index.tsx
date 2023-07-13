2c0c935128a794ac71a513b0e1c63b4a580579d4
bruno santos <devbrunosantos@gmail.com>
2023-07-13 20:23:34 -0300

    feat: styled components install and using

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import theme from "../../theme";
+import { Container, Title } from "./styles";
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container theme={theme}>
+      <Title>Groups</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
